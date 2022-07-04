9417c6251e136a9e58e7b681bc0f53c2f376e54e
Appeltje#5053 <93653477+APPELTJE-SCRIPTS@users.noreply.github.com>
2022-07-04 12:23:41 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,6 @@
 
-author 'FL45H#5053'
-description 'sn-mededeling - stuur een mededeling naar iedereen in je server via een command'
+author 'Appeltje#5053'
+description 'ap-mededeling - stuur een mededeling naar iedereen in je server via een command'
 
 client_scripts {
    'client.lua'
